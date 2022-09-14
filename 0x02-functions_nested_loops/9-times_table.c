300c1332af27873a2a77682da6076309216d4d04
John Njoku <johniyke.njoku@gmail.com>
2022-09-13 19:16:11 -0700

    file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,34 +8,30 @@
 void times_table(void)
 
 {
-
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
+{
+	_putchar('0');
 
-	{
-		_putchar('0');
+	for (mult = 1; mult <= 9; mult++)
+{
+	_putchar(',');
 
-		for (mult = 1; mult <= 9; mult++)
+	_putchar(' ');
 
-		{
-			_putchar(',');
+	prod = num * mult;
 
-			_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
 
-			prod = num * mult;
+	else
+	_putchar((prod / 10) + '0');
 
-			if (prod <= 9)
-
-				_putchar(' ');
-
-			else
-
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
+	_putchar((prod % 10) + '0');
 
 		}
+	_putchar('\n');
 
-		_putchar('\n');
 	}
+}

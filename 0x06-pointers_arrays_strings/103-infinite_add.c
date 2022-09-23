718c00d3e5446b3b80fcf36da5724929209f560e
John Njoku <johniyke.njoku@gmail.com>
2022-09-23 22:11:26 +0000

    task 11 again

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* print_buffer - prints a buffer
-* @b: buffer.
-* @size: size of buffer.
-* Return: no return.
-*/
-void print_buffer(char *b, int size)
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int j, k, l;
+	int i, j, k, l, m, n;
 
-if (size <= 0)
-printf("\n");
-else
-{
-for (j = 0; j < size; j += 10)
-{
-printf("%.8x:", j);
-for (k = j; k < j + 10; k++)
-{
-if (k % 2 == 0)
-printf(" ");
-if (k < size)
-printf("%.2x", *(b + k));
-else
-printf("  ");
-}
-printf(" ");
-for (l = j; l < j + 10; l++)
-{
-if (l >= size)
-break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
-else
-printf("%c", *(b + l));
-}
-printf("\n");
-}
-}
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }

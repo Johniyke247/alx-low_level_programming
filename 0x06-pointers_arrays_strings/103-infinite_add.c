2adbdc51040e16febefd11f262a40bd61751461c
John Njoku <johniyke.njoku@gmail.com>
2022-09-23 02:26:20 +0000

    check

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer.
- * @size: size of buffer.
- * Return: no return.
- */
+* print_buffer - prints a buffer
+* @b: buffer.
+* @size: size of buffer.
+* Return: no return.
+*/
 void print_buffer(char *b, int size)
 {
 int j, k, l;
@@ -14,30 +14,30 @@ int j, k, l;
 if (size <= 0)
 printf("\n");
 else
-	{
-	for (j = 0; j < size; j += 10)
-	{
-	printf("%.8x:", j);
-	for (k = j; k < j + 10; k++)
-	{
-	if (k % 2 == 0)
-	printf(" ");
-	if (k < size)
-	printf("%.2x", *(b + k));
-	else
-	printf("  ");
-	}
-	printf(" ");
+{
+for (j = 0; j < size; j += 10)
+{
+printf("%.8x:", j);
+for (k = j; k < j + 10; k++)
+{
+if (k % 2 == 0)
+printf(" ");
+if (k < size)
+printf("%.2x", *(b + k));
+else
+printf("  ");
+}
+printf(" ");
 for (l = j; l < j + 10; l++)
-	{
-	if (l >= size)
-	break;
-	if (*(b + l) < 32 || *(b + l) > 126)
-	printf("%c", '.');
-	else
-	printf("%c", *(b + l));
-	}
-	printf("\n");
-		}
-	}
+{
+if (l >= size)
+break;
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b + l));
+}
+printf("\n");
+}
+}
 }
